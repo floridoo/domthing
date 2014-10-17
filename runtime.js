73105c56d2e02288a260972c9fe446a199aa2bbd
Florian Reiterer <me@florianreiterer.com>
2014-10-17 16:33:29 +0200

    changes for more flexibility, with helper

--- runtime.js
+++ runtime.js
@@ -1,10 +1,12 @@
 var Template = require('./lib/runtime/template');
+var Context = require('./lib/runtime/context');
 var hooks = require('./lib/runtime/hooks');
 var helpers = require('./lib/runtime/helpers');
 var expressions = require('./lib/runtime/expressions');
 
 module.exports = {
     Template: Template,
+    Context: Context,
     hooks: hooks,
 
     registerHelper: helpers.registerHelper,
